0503ec92d46bad03e781ba63697fa2ad77d7540f
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-20 09:10:34 +0200

    Commit Laura

--- src/ej_contornos/Ej_contornos.java
+++ src/ej_contornos/Ej_contornos.java
@@ -6,9 +6,8 @@ public class Ej_contornos {
 
   
     public static void main(String[] args) {
-        
-        
         System.out.println("Laura, creo que voy a robarle los rotus a Tahía, que tiene muchos muajajaja");
+        System.out.println("Menuda ladrona!! Si le pillas dame algunos a mi :)");
     }
     
 }

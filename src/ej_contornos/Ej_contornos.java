79012690ab0b162e1ef01a9b3580fb1719996e83
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-10-20 09:40:38 +0200

    Modificacion Miriam

--- src/ej_contornos/Ej_contornos.java
+++ src/ej_contornos/Ej_contornos.java
@@ -7,7 +7,7 @@ public class Ej_contornos {
   
     public static void main(String[] args) {
         System.out.println("Laura, creo que voy a robarle los rotus a Tahía, que tiene muchos muajajaja");
-        System.out.println("Menuda ladrona!! Si le pillas dame algunos a mi :)");
+        System.out.println("Haces bien!! Si le pillas dame algunos a mi :)");
     }
     
 }

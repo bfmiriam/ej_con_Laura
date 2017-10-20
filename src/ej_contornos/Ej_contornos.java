ce9b7caa4d31e48104f497f2813f6ce60404d558
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-10-20 09:04:57 +0200

    Rotus Tahia

---
+++ src/ej_contornos/Ej_contornos.java
@@ -0,0 +1,14 @@
+
+package ej_contornos;
+
+
+public class Ej_contornos {
+
+  
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Laura, creo que voy a robarle los rotus a Tahía, que tiene muchos muajajaja");
+    }
+    
+}
